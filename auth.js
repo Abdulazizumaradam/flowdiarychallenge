76a41b80d195bc4f38f13a77581c47f9cab6e66a
Abdulazizumaradam <abdulazizu6024@gmail.com>
2025-05-21 14:03:14 +0100

    Authentication Form

--- auth.js
+++ auth.js
@@ -1,1 +1,21 @@
-console.log("Abdulaziz")
+function validateAuthForm(){
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const error = document.getElementById("error");
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+    if(email==="" || password===""){
+        error.textContent = "Please Fill in all fields";
+
+        return false;
+    }
+
+    if(!emailPatttern.test(email)){
+        error.textContent = "Please enter a valid email address";
+        return false;
+    }
+
+    error.textContent = "";
+    return true;
+    
+}
